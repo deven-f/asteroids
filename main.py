ae9b26a5f6cc3bb3536cdad4016e394a854fffcf
Deven <devhfos@gmail.com>
2025-02-25 23:21:37 +0000

    clean up leftover stuff from testing

--- main.py
+++ main.py
@@ -4,19 +4,14 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    background_colour = pygame.Color("black")
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(background_colour)
+        
+        screen.fill("black")
         pygame.display.flip()
-
-
-   # print("Starting Asteroids!")
-   # print(f"Screen width: {SCREEN_WIDTH}")
-   # print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
